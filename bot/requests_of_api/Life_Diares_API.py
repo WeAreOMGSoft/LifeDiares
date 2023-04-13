cce6a70dd174247dca1ff965a3b0e254e028e7f2
SagidKamilov <89615854074a@gmail.com>
2023-04-13 07:07:19 +0300

    Добавил изменил LifeDiaresAPI

--- bot/requests_of_api/Life_Diares_API.py
+++ bot/requests_of_api/Life_Diares_API.py
@@ -3,13 +3,13 @@ import json
 
 class API:
     def __init__(self):
-        self.adr = 'https://localhost:8080'
+        self.url = ''
         self.session = requests.Session()
 
     def login(self, user_id: int) -> json:
         params = {"user_id": user_id}
-        return self.session.get(url=self.adr+'/login', params=params).json()
+        return self.session.get(url=self.url + '/login', params=params).json()
 
     def register(self, user_id: int) -> json:
         params = {"user_id": user_id}
-        return self.session.get(url=self.adr+'/register', params=params).json()
+        return self.session.get(url=self.url + '/register', params=params).json()
